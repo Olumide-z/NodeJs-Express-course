764c455cb650dfcf6d46b18ab375421a18ae34d1
Olumide-Z <ilesanmiolumide10@gmail.com>
2023-08-27 00:20:29 +0100

    node and express course

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,64 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+require('express-async-errors');
+// express
+
+const express = require('express');
+const app = express();
+// rest of packages
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+// database
+const connectDB = require('./db/connect');
+
+//  routers
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.get('/', (req, res) => {
+    res.send('ecommerce api')
+});
+
+app.use(morgan('tiny'))
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use(express.static('./public'));
+app.use(fileUpload());
+
+app.get('/', (req, res) => {
+    res.send('e-commerce api')
+})
+
+app.get('/api/v1', (req, res) => {
+    res.send('e-commerce api')
+    console.log(req.signedCookies)
+})
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter)
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware)
+
+
+const PORT = process.env.PORT || 5000
+const start = async () => {
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, console.log(`server running on port ${PORT}`))
+    }catch(error){
+        console.log(error)
+    }
+};
+
+start();
